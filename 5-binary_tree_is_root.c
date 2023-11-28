2f6607e97de0eaa928cee66b7bb131f492e986b4
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-11-28 18:24:39 +0100

    binary trees

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: pointer to the node to be checked
- * 
+ *
  * Return: 1 if node is root, 0 otherwise.
  */
 int binary_tree_is_root(const binary_tree_t *node)
@@ -14,5 +14,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node->parent == NULL)
 		return (1);
 	else
-		return(0);
+		return (0);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
